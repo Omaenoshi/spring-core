e8fd3787cd0a57a45f60d3455258605f52c852b0
Omaenoshi <71465546+Omaenoshi@users.noreply.github.com>
2022-11-11 16:50:46 +0500

    Initial commit

---
+++ src/main/java/com/yet/spring/Client.java
@@ -0,0 +1,28 @@
+package com.yet.spring;
+
+public class Client {
+
+    private long id;
+    private String fullName;
+
+    public Client(long id, String fullName) {
+        this.id = id;
+        this.fullName = fullName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+}
---
+++ src/main/java/com/yet/spring/ConsoleEventLogger.java
@@ -0,0 +1,8 @@
+package com.yet.spring;
+
+public class ConsoleEventLogger {
+
+    public void logEvent(String msg) {
+        System.out.println(msg);
+    }
+}
